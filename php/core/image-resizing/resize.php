7b37afb22f1f4e8dd193bf89bb0468be69c9a8fd
Aman Kharbanda <amankharbanda34@gmail.com>
2021-10-22 21:51:30 +0530

    added dest for resize functions

--- php/core/image-resizing/resize.php
+++ php/core/image-resizing/resize.php
@@ -1,23 +1,23 @@
 <?php
 
 
-function processjpg($filename)
+function processjpg($filename, $dest)
 {
     list($width, $height) = getimagesize($filename);
     $newheight = (200 * $height) / $width;
     $imagetruecolor = imagecreatetruecolor(200, $newheight);
     $newimage = imagecreatefromjpeg($filename);
     imagecopyresampled($imagetruecolor, $newimage, 0, 0, 0, 0, 200, $newheight, $width, $height);
-    imagejpeg($imagetruecolor, 'newjpg.jpg', 80);
+    imagejpeg($imagetruecolor, $dest, 80);
     echo $filename . " Processed";
 };
-function processpng($filename)
+function processpng($filename, $dest)
 {
     list($width, $height) = getimagesize($filename);
     $newheight = (200 * $height) / $width;
     $imagetruecolor = imagecreatetruecolor(200, $newheight);
     $newimage = imagecreatefrompng($filename);
     imagecopyresampled($imagetruecolor, $newimage, 0, 0, 0, 0, 200, $newheight, $width, $height);
-    imagepng($imagetruecolor, 'newpng.png', 8);
+    imagepng($imagetruecolor, $dest, 8);
     echo $filename . " Processed";
 };
